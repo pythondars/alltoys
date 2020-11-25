f92686f78303b460a99d3ba22f0dc7bf8ab07a21
Ibrohim Ermatov <ibrohimbek@gmail.com>
2020-11-25 11:48:07 +0500

    Connect to the postgres database

--- alltoys/settings.py
+++ alltoys/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'alltoys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "all_toys",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
     }
 }
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
